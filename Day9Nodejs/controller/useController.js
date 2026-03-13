321936461373ca565994523605efb10a7194bfc9
Raghvendra Chauhan <raghvch1122@gmail.com>
2026-03-13 10:57:14 +0530

    13 March Changes

--- Day9Nodejs/controller/useController.js
+++ Day9Nodejs/controller/useController.js
@@ -2,8 +2,14 @@ const users = require('../module/useModule');
 
 
 exports.getUsers =(req,res)=>{
+    const clientQuery = req.query;
+    console.log("clientQuery: ",clientQuery)
     res.json(users)
-}
+};
+exports.addUser = (req, res) => {
+    console.log("req: ",req.body);
+    res.json(req.body);
+};
 exports.getUsersById =(req,res)=>{
     const userId = req.params.id;
     users.map(userDetail=>{
